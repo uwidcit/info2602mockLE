bf775f4f98f7c57e98bb5dafcce7ba8cb4dd94b6
Nicholas Mendez <snickdx@gmail.com>
2022-03-21 19:37:39 +0000

    implemented solution

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
 from flask_cors import CORS
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, url_for, redirect, send_from_directory
 from sqlalchemy.exc import IntegrityError
 
-from models import db
+from models import db, Log
 
 ''' Begin boilerplate code '''
 def create_app():
@@ -21,13 +21,79 @@ app.app_context().push()
 
 ''' End Boilerplate Code '''
 
-@app.route('/')
-def index():
-  return 'Hello World!'
+'''SSR Version '''
 
-@app.route('/app')
-def client_app():
-  return app.send_static_file('app.html')
+@app.route('/', methods=['GET'])
+def index():
+  logs = Log.query.all()
+  return render_template('index.html', logs=logs)
+
+@app.route('/edit/<id>', methods=['GET'])
+def edit_page(id):
+  return render_template('edit.html', id=id)
+
+@app.route('/edit/<id>', methods=['POST'])
+def edit_action(id):
+  data = request.form
+  log = Log.query.get(id)
+  if log:
+    log.stream = data['stream']
+    db.session.add(log)
+    db.session.commit()
+  return redirect(url_for('index'))
+
+@app.route('/logs', methods=['POST'])
+def create_log():
+  data = request.form
+  newlog = Log(stdid=data['stdid'], stream=data['stream'])
+  db.session.add(newlog)
+  db.session.commit()
+  return redirect(url_for('index'))
+
+# returns a count of how many times a particular stream appears in the table
+@app.route('/stats', methods=['GET'])
+def stats():
+  logs = Log.query.all()
+  stats = [0, 0, 0]
+  for log in logs:
+    stats[log.stream - 1] += 1
+  return jsonify(stats)
+
+''' END SSR Version '''
+
+
+''' Begin CSR Version '''
+
+@app.route('/app', methods=['GET'])
+def home():
+  return send_from_directory('static', 'index.html')
+
+@app.route('/api/logs', methods=['GET'])
+def get_log():
+  logs = Log.query.all()
+  logs = [ log.toDict() for log in logs]
+  return jsonify(logs)
+
+@app.route('/api/logs/<id>', methods=['PUT'])
+def update_log(id):
+  data = request.json
+  log = Log.query.get(id)
+  if log:
+    log.stream = data['stream']
+    db.session.add(log)
+    db.session.commit()
+  return jsonify({"message": f'Log with id {log.id} updated!'}), 201
+
+@app.route('/api/logs', methods=['POST'])
+def create_log2():
+  data = request.json
+  newlog = Log(stdid=data['stdid'], stream=int(data['stream']))
+  db.session.add(newlog)
+  db.session.commit()
+  return jsonify({"message": f'Log created with id {newlog.id}'}), 201
+
+
+''' END CSR Version '''
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,2 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 db = SQLAlchemy()
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    stdid = db.Column(db.String(10), nullable=False)
+    stream = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.datetime.now(), nullable=False)
+
+    def toDict(self):
+        return {
+            'id': self.id,
+            'stdid': self.stdid,
+            'stream': self.stream,
+            'date': self.date.strftime("%Y/%m/%d, %H:%M:%S")
+        }
